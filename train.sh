3b7e63b49678c72b6cb311722f4ad04d3a895611
ideallove <20013535+ideallove@users.noreply.github.com>
2024-05-05 16:23:39 +0800

    test

--- train.sh
+++ train.sh
@@ -1,19 +1,20 @@
-# nohup ./train.sh > mylog_240429.log 2>&1 &
-DATE=240429
+# nohup ./train.sh > mylog_240430.log 2>&1 &
+DATE=240430
 CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --stage sft \
     --do_train True \
-    --model_name_or_path /home/dyf/model/Llama-2-7b-chat-hf-modified \
     --finetuning_type lora \
-    --template self_rog_llama2 \
+    --model_name_or_path /home/dyf/model/Phi-3-mini-128k-instruct-modified \
+    --template phi_self \
+    --output_dir /home/finetune/phi_rog_${DATE}_01 \
     --dataset_dir data \
     --dataset rog_train_all_shuffled \
     --cutoff_len 2048 \
     --preprocessing_num_workers 1 \
     --num_train_epochs 3.0 \
     --max_samples 120000 \
-    --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 16 \
     --lr_scheduler_type cosine \
     --max_grad_norm 1.0 \
     --logging_steps 25 \
@@ -35,7 +36,7 @@ CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --split_special_tokens True \
     --additional_target embed_tokens,lm_head,norm \
     --use_cache False \
-    --output_dir /home/finetune/rog_${DATE}_02 \
+    --flash_attn fa2 \
     | tee -a mylog_tee_${DATE}.log
 
 #    --lora_dropout 0.05 \
