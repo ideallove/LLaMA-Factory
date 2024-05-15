70cb9465a11457e5e7a1828b8d97ebc2da0d301b
ideallove <20013535+ideallove@users.noreply.github.com>
2024-05-15 18:52:59 +0800

    test

--- my/single_node.sh
+++ my/single_node.sh
@@ -1,3 +1,5 @@
+# nohup ./my/single_node.sh > mylog_240515.log 2>&1 &
+
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch \
     --config_file my/accelerate/single_config.yaml \
     src/train.py my/llama3_lora_sft.yaml
